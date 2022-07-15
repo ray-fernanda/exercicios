1f2284b9dbb319769faf43b7e164ef2581af11db
Rayane Fernanda <rayanefernanda12@hotmail.com>
2022-07-15 16:37:26 -0300

    atualizando código

--- ex2.c
+++ ex2.c
@@ -3,18 +3,29 @@
 #include <math.h>
 #include <stdbool.h>
 	
-bool isPerfectSquare(int x) {
+bool quadradoPerfeito(int x) {
     int s = sqrt(x);
     return (s * s == x);
 }
-	
-bool isFibonacci(int n) {
-    return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4);
+ 
+bool Fibonacci(int n) {
+    return quadradoPerfeito(5 * n * n + 4) || quadradoPerfeito(5 * n * n - 4);
 }
-	
-int main() { 
+ 
+int main() {
 	setlocale(LC_ALL, "portuguese");
-		
+	
+    int num;
+    
+    printf("Digite um número: ");
+    scanf("%i",&num);
+	
+	if(Fibonacci(num)) {
+		printf("Este número pertence a sequência!\n");
+	} else {
+		printf("Este número não pertence a sequência!\n");
+	}
+	
 	return 0;
-}	
-
+}
+ 
